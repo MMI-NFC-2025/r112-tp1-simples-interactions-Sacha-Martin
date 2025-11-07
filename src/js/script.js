ea2ca280c8996fa1cf819e4c3ea5e29d3b8bbf00
Sacha-Martin <sacha.martin03@edu.univ-fcomte.fr>
2025-11-07 14:21:26 +0100

    simples interactions

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,7 @@
 // TODO
+const aMettreEnRouge = document.querySelector("#a-mettre-en-rouge")
+aMettreEnRouge.style.color = "red"
+const EnRougeSuiteAClick = document.querySelector('#en-rouge-suite-a-click')
+EnRougeSuiteAClick.addEventListener("click",(evt)=>{
+        EnRougeSuiteAClick.style.color = 'red'
+});
