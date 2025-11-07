c398ab13b62e344fee810d1a27a5a708b574bddc
Sacha-Martin <sacha.martin03@edu.univ-fcomte.fr>
2025-11-07 14:32:39 +0100

    Coll.éléments

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,10 @@ const EnRougeSuiteAClick = document.querySelector('#en-rouge-suite-a-click')
 EnRougeSuiteAClick.addEventListener("click",(evt)=>{
         EnRougeSuiteAClick.style.color = 'red'
 });
+
+const lesh2 = document.querySelectorAll("h2");
+lesh2.forEach((elem) => {
+  elem.addEventListener("click", (evt) => {
+    evt.target.style.color = "red";
+  });
+});
